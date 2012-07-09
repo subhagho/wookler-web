a0d608b7b9119c6633390d070b68eea2d755edbe
Deepak Sharma <wookler@Deepaks-iMac.local>
2012-07-08 20:17:17 -0700

    control embedded

--- agile_carousel_data.php
+++ agile_carousel_data.php
@@ -1,16 +1,13 @@
 [{
-      "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='images/banner_bike.jpg' alt='Bike'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div>",
-      "content_button": "<div class='thumb'><img src='images/f2_thumb.jpg' alt='bike is nice'></div><p>Agile Carousel Place Holder</p>"
+      "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='../resources/sample1.jpg' alt='Bike'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div>",
+      "content_button": "<div class='thumb'><img src='../resources/samplethumb.jpg' alt='bike is nice'></div><p>Thunderbolt Series</p>"
 }, {
-      "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='images/banner_paint.jpg' alt='Paint'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div>",
-      "content_button": "<div class='thumb'><img src='images/f2_thumb.jpg' alt='bike is nice'></div><p>Agile Carousel Place Holder</p>"
+      "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='../resources/sample2.jpg' alt='Paint'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div>",
+      "content_button": "<div class='thumb'><img src='../resources/samplethumb1.jpg' alt='bike is nice'></div><p>Lightning Series</p>"
 }, {
-      "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='images/banner_tunnel.jpg' alt='Tunnel'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div>",
-      "content_button": "<div class='thumb'><img src='images/f2_thumb.jpg' alt='bike is nice'></div><p>Agile Carousel Place Holder</p>"
+      "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='../resources/sample3.jpg' alt='Tunnel'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div>",
+      "content_button": "<div class='thumb'><img src='../resources/samplethumb2.jpg' alt='bike is nice'></div><p>Lion Series</p>"
 }, {
-      "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='images/banner_bike.jpg' alt='Bike'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div>",
-      "content_button": "<div class='thumb'><img src='images/f2_thumb.jpg' alt='bike is nice'></div><p>Agile Carousel Place Holder</p>"
-}, {
-      "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='images/banner_paint.jpg' alt='Paint'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div>",
-      "content_button": "<div class='thumb'><img src='images/f2_thumb.jpg' alt='bike is nice'></div><p>Agile Carousel Place Holder</p>"
+      "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='../resources/sample4.jpg' alt='Bike'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div>",
+      "content_button": "<div class='thumb'><img src='../resources/samplethumb3.jpg' alt='bike is nice'></div><p>Cheetah Series</p>"
 }]
